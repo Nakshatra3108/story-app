30a90caffcfc745d5a26088d809a7a91b86de5de
Naksh3108 <79451370+Nakshatra3108@users.noreply.github.com>
2024-05-17 16:21:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAT8eBMwPHStgMZvBEd9B2hRGcunFAvRzw",
+  authDomain: "storytelling-app-7275a.firebaseapp.com",
+  databaseURL: "https://storytelling-app-7275a-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-7275a",
+  storageBucket: "storytelling-app-7275a.appspot.com",
+  messagingSenderId: "61648551658",
+  appId: "1:61648551658:web:357e274afd08083bdaa193"
+  }
